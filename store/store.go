6dc5165c8dd4c5cbada15c7d450c67afdc809337
Tuong Le <tuongaz@gmail.com>
2024-05-15 14:18:18 +1000

    clean up

--- store/store.go
+++ store/store.go
@@ -51,3 +51,15 @@ func (s *Store) Close() error {
 
 	return nil
 }
+
+type DBError struct {
+	Err error
+}
+
+func NewDBError(err error) *DBError {
+	return &DBError{Err: err}
+}
+
+func (e *DBError) Error() string {
+	return e.Err.Error()
+}
