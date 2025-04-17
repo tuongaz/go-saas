4d62704983a99896e906cfd471e4368641e49e5d
Tuong Le <tuongaz@gmail.com>
2025-04-18 09:47:04 +1000

    [skip ci] wip

--- server/middleware/ratelimit.go
+++ server/middleware/ratelimit.go
@@ -8,13 +8,7 @@ import (
 )
 
 // RateLimiterMiddleware returns a middleware that limits request per second
-// for each route. The default rate is 15 requests per second.
-func RateLimiterMiddleware() func(http.Handler) http.Handler {
-	return httprate.LimitByIP(15, time.Second)
-}
-
-// RateLimiterWithOptions returns a middleware with custom options
-// for more advanced rate limiting scenarios
-func RateLimiterWithOptions(requestsLimit int, windowLength time.Duration) func(http.Handler) http.Handler {
+// for each route.
+func RateLimiterMiddleware(requestsLimit int, windowLength time.Duration) func(http.Handler) http.Handler {
 	return httprate.LimitByIP(requestsLimit, windowLength)
 }
